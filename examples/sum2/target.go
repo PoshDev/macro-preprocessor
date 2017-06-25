93817229fcb8e49174482b3ebaa9c4964e026002
Ken Leidal <kkleidal@gmail.com>
2017-06-24 20:27:38 -0400

    put autogenerated code warning in

--- examples/sum2/target.go
+++ examples/sum2/target.go
@@ -1,3 +1,10 @@
+// .----------------------------------------.
+// |      WARNING: AUTO-GENERATED CODE      |
+// .----------------------------------------.
+// This code was automatically generated using
+// https://github.com/PoshDev/macro-preprocessor .
+// The original source file was source.go.
+// XXX: DO NOT CHANGE THIS FILE!!!
 package main
 
 
